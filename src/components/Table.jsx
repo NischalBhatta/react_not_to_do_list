e54ea8b3bd11d0724e1400225bbb6288357f5890
NischalBhatta <nischalpb@gmail.com>
2026-08-19 20:30:53 +1000

    Fixed the UI to hide the list table if the list is empty

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -22,95 +22,104 @@ export const Table = ({
           {/* <!-- Entry List --> */}
           <h2>Entry List</h2>
           <hr />
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="allEntry"
-            id="all-entry"
-            onChange={handleOnSelect}
-          />
-          {""}
-          <label htmlFor="all-entry">Select All</label>
-          <table className="table  table-borderless">
-            <tbody id="entryList">
-              {entryList.map((item, i) => {
-                return (
-                  <tr className="border pb-2 " key={item?._id}>
-                    <td>{i + 1}</td>
-                    <td>
-                      <input
-                        className="form-check-input"
-                        type="checkbox"
-                        value={item._id}
-                        onChange={handleOnSelect}
-                        checked={toDelete.includes(item?._id)}
-                      />
-                      {""}
-                      {item.task}
-                    </td>
-                    <td>{item.hr}hrs</td>
-                    <td className="d-flex justify-content-end">
-                      <div className="">
-                        <button className="btn btn-warning">
-                          <i
-                            className="fa-solid fa-arrow-right"
-                            onClick={() => switchTask(item._id, "bad")}
-                          ></i>
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          {entryList.length > 0 && (
+            <div>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="allEntry"
+                id="all-entry"
+                onChange={handleOnSelect}
+              />
+              {""}
+              <label htmlFor="all-entry">Select All</label>
+              <table className="table  table-borderless">
+                <tbody id="entryList">
+                  {entryList.map((item, i) => {
+                    return (
+                      <tr className="border pb-2 " key={item?._id}>
+                        <td>{i + 1}</td>
+                        <td>
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value={item._id}
+                            onChange={handleOnSelect}
+                            checked={toDelete.includes(item?._id)}
+                          />
+                          {""}
+                          {item.task}
+                        </td>
+                        <td>{item.hr}hrs</td>
+                        <td className="d-flex justify-content-end">
+                          <div className="">
+                            <button className="btn btn-warning">
+                              <i
+                                className="fa-solid fa-arrow-right"
+                                onClick={() => switchTask(item._id, "bad")}
+                              ></i>
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
         <div className="col bg-subtle text-center">
           {/* <!-- Bad List --> */}
           <h2>Bad List</h2>
           <hr />
-          <input
-            className="form-check-input"
-            type="checkbox"
-            value="allBad"
-            id="all-bad"
-            onChange={handleOnSelect}
-          />
-          {""}
-          <label htmlFor="all-bad">Select All</label>
-          <table className="table table-hover table-borderless">
-            <tbody id="badList">
-              {badList.map((item, i) => (
-                <tr className="border pb-2" key={item?._id}>
-                  <td>{i + 1}</td>
+          {badList.length > 0 && (
+            <div>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="allBad"
+                id="all-bad"
+                onChange={handleOnSelect}
+              />
+              {""}
 
-                  <td>
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value={item?._id}
-                      id=""
-                      onChange={handleOnSelect}
-                      checked={toDelete.includes(item?._id)}
-                    />
-                    {""}
-                    {item.task}
-                  </td>
-                  <td>{item.hr}hrs</td>
-                  <td className="d-flex justify-content-end">
-                    <div className="">
-                      <button className="btn btn-warning">
-                        <i
-                          className="fa-solid fa-arrow-left"
-                          onClick={() => switchTask(item._id, "entry")}
-                        ></i>
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+              <label htmlFor="all-bad">Select All</label>
+              <table className="table table-hover table-borderless">
+                <tbody id="badList">
+                  {badList.map((item, i) => (
+                    <tr className="border pb-2" key={item?._id}>
+                      <td>{i + 1}</td>
+
+                      <td>
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          value={item?._id}
+                          id=""
+                          onChange={handleOnSelect}
+                          checked={toDelete.includes(item?._id)}
+                        />
+                        {""}
+                        {item.task}
+                      </td>
+                      <td>{item.hr}hrs</td>
+                      <td className="d-flex justify-content-end">
+                        <div className="">
+                          <button className="btn btn-warning">
+                            <i
+                              className="fa-solid fa-arrow-left"
+                              onClick={() => switchTask(item._id, "entry")}
+                            ></i>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
 
           <div className="alert alert-light">
             You could have saved{" "}
